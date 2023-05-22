4ac265a8265dbf615aa519933466bc952d0f6147
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-05-22 23:33:41 +0100

    links

--- src/Components/Navbar/Link.tsx
+++ src/Components/Navbar/Link.tsx
@@ -11,8 +11,8 @@ const Link = ({page, selectedPage, setSelectedPage}: Props) => {
     const lowerCasePage = page.toLowerCase().replace(/ /g, '')
     return (
        <AnchorLink
-        className={`${selectedPage === lowerCasePage ? 'text-primary-500' : ''}
-        transition duration-500 hover:text-primary-300`}
+        className={`${selectedPage === lowerCasePage ? 'text-primary-600' : ''}
+        transition duration-500 hover:text-primary-400`}
         href={`#${lowerCasePage}`}
         onClick={() => setSelectedPage(lowerCasePage)}
        >
